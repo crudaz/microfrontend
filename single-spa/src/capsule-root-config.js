2df71087d0476825b19207d08bcc19c33dfbb800
crudaz <romerocruz@gmail.com>
2021-04-07 20:24:53 -0500

    feat: single root app

---
+++ single-spa/src/capsule-root-config.js
@@ -0,0 +1,20 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: (location) => location.pathname === '/' ,
+});
+
+registerApplication({
+  name: "@capsule/react-single",
+  app: () => System.import("@capsule/react-single"),
+  activeWhen: (location) => location.pathname === '/react-single',
+});
+
+start({
+  urlRerouteOnly: true,
+});
